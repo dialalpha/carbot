5841b19a6fe2f2f5ead4c4595df97312ceea0dd3
ALPHA-YOGA910\Alpha <dial.alpha@gmail.com>
2017-03-14 18:56:21 -0400

    add ILogger interface

---
+++ Carbot.Common/ILogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Carbot.Common
+{
+    public interface ILogger
+    {
+        void LogTrace(string format, params string[] args);
+        void LogDebug(string format, params string[] args);
+        void LogInfo(string format, params string[] args);
+        void LogWarn(string format, params string[] args);
+        void LogError(string format, params string[] args);
+        void LogFatal(string format, params string[] args);
+        void LogException(Exception ex);
+    }
+}
